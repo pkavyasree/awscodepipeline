3520d4c75aab7f7daaa9c4111ca77bd71459a73f
pkavyasree <kavya.panchumarthy@gmail.com>
2019-01-18 15:18:57 +0530

    Update lambda_function.py

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -1,7 +1,3 @@
-import boto3
-import json
-
-ec2 = boto3.client('ec2')
-def lambda_handler(event, context):
-    response = ec2.describe_availability_zones()
-    return {"statusCode": 200, "body": json.dumps(response)}
+def lambda_handler(event,context):
+    print("This is my test lambda function")
+    return "Hello from lambda"
