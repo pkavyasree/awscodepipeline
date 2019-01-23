01916c14959bf27b54af8fdb9de52094b199a736
pkavyasree <kavya.panchumarthy@gmail.com>
2019-01-23 18:33:07 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event,context):
     print("This is my first lambda function")
     return "Hello from lambda"
-eventfunction = event1
-Handlername = lambda_function.lambda_handler
+#eventfunction = event1
+#Handlername = lambda_function.lambda_handler
