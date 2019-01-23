2df61f42eb3021ffc941ec5058f32929eb12a740
pkavyasree <kavya.panchumarthy@gmail.com>
2019-01-23 13:26:39 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,14 @@
-import boto3
-import json
-
-ec2 = boto3.client('ec2')
-def lambda_handler(event, context):
-    response = ec2.describe_availability_zones()
-    return {"statusCode": 200, "body": json.dumps(response)}
+b = 30                                  
+def display():                          
+    print("I am one")
+    global a
+    a = 20
+    b = 25
+    print(b)
+def dispaly2():
+    print("I am two")
+print(b)
+if __name__ == '__main__':
+    display()
+print(a)
+print(b)
