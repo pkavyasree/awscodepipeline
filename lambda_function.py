4372b55b698a057cb75d55314c94312cf363af24
pkavyasree <kavya.panchumarthy@gmail.com>
2019-01-28 15:25:03 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,11 @@
-import boto3
 import json
+import boto3
+ec2 = boto3.client('ec2',region_name="us-east-1")
 
-ec2 = boto3.client('ec2',region_name="us-east-1",aws_access_key_id="AKIAIUIGD4R2ZEXJUP5A",aws_secret_access_key="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")
 def lambda_handler(event, context):
+    # TODO implement
     response = ec2.describe_availability_zones()
-    return {"statusCode": 200, "body": json.dumps(response)}
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
