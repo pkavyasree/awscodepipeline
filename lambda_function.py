a6aa0f5904c5eb7ba32568f0eee132ea5a5e86d2
pkavyasree <kavya.panchumarthy@gmail.com>
2019-01-28 13:24:03 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,34 @@
-import json
+import boto3
 
-def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+session = boto3.Session(region_name="us-east-1")
+
+ec2 = session.resource('ec2')
+
+def running():
+
+    print("The Running instances are")
+
+    rinstances = ec2.instances.filter(
+
+        Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+
+    for instance in rinstances:
+
+        print(instance.id, instance.instance_type)
+
+def stopped():
+
+    print("The Stopped instances are")
+
+    sinstances = ec2.instances.filter(
+
+        Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}])
+
+    for instance1 in sinstances:
+
+        print(instance1.id, instance1.instance_type)
+
+running()
+
+stopped()
+
