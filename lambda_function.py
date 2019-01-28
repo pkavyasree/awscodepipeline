86b8127f1aafe6ef081341e0d4376e75e1704281
pkavyasree <kavya.panchumarthy@gmail.com>
2019-01-28 13:42:48 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,34 +1,7 @@
 import boto3
+import json
 
-session = boto3.Session(region_name="us-east-1")
-
-ec2 = session.resource('ec2')
-
-def running():
-
-    print("The Running instances are")
-
-    rinstances = ec2.instances.filter(
-
-        Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
-
-    for instance in rinstances:
-
-        print(instance.id, instance.instance_type)
-
-def stopped():
-
-    print("The Stopped instances are")
-
-    sinstances = ec2.instances.filter(
-
-        Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}])
-
-    for instance1 in sinstances:
-
-        print(instance1.id, instance1.instance_type)
-
-running()
-
-stopped()
-
+ec2 = boto3.client('ec2',region_name="us-east-1",aws_access_key_id="AKIAIUIGD4R2ZEXJUP5A",aws_secret_access_key="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")
+def lambda_handler(event, context):
+    response = ec2.describe_availability_zones()
+    return {"statusCode": 200, "body": json.dumps(response)}
